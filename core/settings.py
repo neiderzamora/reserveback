8f839615fc37cdd5508842ff4836c848b73efecd
ihuntersouls <neiderzamoramoreno@gmail.com>
2023-05-31 13:24:38 -0500

    ajustes

--- core/settings.py
+++ core/settings.py
@@ -204,9 +204,6 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    #os.path.join(BASE_DIR, 'frontend/build/static')
-]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
